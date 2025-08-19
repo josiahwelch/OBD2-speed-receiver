10ef349caf5585c0735d38d2843cdaf9a7947e18
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-08-19 00:57:23 -0500

    changed Serial to SerialUSB

--- LoRaFirmware/LoRaFirmware.ino
+++ LoRaFirmware/LoRaFirmware.ino
@@ -2,26 +2,27 @@
 #include <LoRa.h>
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+  SerialUSB.begin(9600);
+  while (!SerialUSB);
 
   LoRa.setPins(8, 4, 3); // NSS, RESET, DIO0 – adjust to your wiring
 
   if (!LoRa.begin(915E6)) {
-    Serial.println("LoRa init failed!");
+    SerialUSB.println("LoRa init failed!");
     while (1);
   }
-  Serial.println("LoRa Receiver");
+  SerialUSB.println("LoRa Receiver");
 }
 
 void loop() {
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
-    Serial.print("Received: ");
+    SerialUSB.print("Received: ");
     while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
+      SerialUSB.print((char)LoRa.read());
     }
-    Serial.println();
+    SerialUSB.println("not received");
   }
+  delay(100);
 }
 
